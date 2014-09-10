7aefc105227ae8dd5675d70baced34933c3d43fd
Danylo Bilyk <danylo.bilyk@accenture.com>
2014-09-10 13:35:30 +0300

    Eclipse CDT4 - Unix Makefiles

---
+++ CMakeFiles/MyGame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyGame.
+# This may be replaced when dependencies are built.
---
+++ cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Animation.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Animation.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/AnimationState.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/AnimationState.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/AnimationStateData.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/AnimationStateData.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Atlas.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Atlas.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/AtlasAttachmentLoader.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/AtlasAttachmentLoader.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Attachment.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Attachment.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/AttachmentLoader.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/AttachmentLoader.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Bone.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Bone.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/BoneData.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/BoneData.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/BoundingBoxAttachment.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/BoundingBoxAttachment.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CCSkeleton.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/CCSkeleton.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CCSkeletonAnimation.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/CCSkeletonAnimation.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Event.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Event.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/EventData.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/EventData.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Json.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Json.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/RegionAttachment.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/RegionAttachment.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Skeleton.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Skeleton.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/SkeletonBounds.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/SkeletonBounds.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/SkeletonData.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/SkeletonData.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/SkeletonJson.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/SkeletonJson.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Skin.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Skin.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/Slot.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/Slot.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/SlotData.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/SlotData.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/extension.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/extension.cpp.o"
+  "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/spine-cocos2dx.cpp" "/home/antidotcb/workspace/match3/cocos2d/cocos/editor-support/spine/CMakeFiles/spine.dir/spine-cocos2dx.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "CC_ENABLE_CHIPMUNK_INTEGRATION=1"
+  "LINUX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/GLFW"
+  "/usr/include/GLFW"
+  "cocos2d"
+  "cocos2d/cocos"
+  "cocos2d/cocos/audio/include"
+  "cocos2d/cocos/platform"
+  "cocos2d/cocos/platform/desktop"
+  "cocos2d/cocos/platform/linux"
+  "cocos2d/cocos/editor-support"
+  "cocos2d/extensions"
+  "cocos2d/external"
+  "cocos2d/external/edtaa3func"
+  "cocos2d/external/jpeg/include/linux"
+  "cocos2d/external/tiff/include/linux"
+  "cocos2d/external/webp/include/linux"
+  "cocos2d/external/tinyxml2"
+  "cocos2d/external/unzip"
+  "cocos2d/external/freetype2/include/linux"
+  "cocos2d/external/websockets/include/linux"
+  "cocos2d/external/spidermonkey/include/linux"
+  "cocos2d/external/linux-specific/fmod/include/64-bit"
+  "cocos2d/external/xxhash"
+  "cocos2d/external/chipmunk/include/chipmunk"
+  "cocos2d/cocos/editor-support/spine/.."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
